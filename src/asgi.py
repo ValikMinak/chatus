4be584856c80fdfa682a723bcd46d14c00c5fa75
paul <pavel@esmedia.es>
2022-03-05 00:10:06 +0200

    up and running

--- src/asgi.py
+++ src/asgi.py
@@ -13,10 +13,11 @@ import django
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 django.setup()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": AsgiHandler(),
