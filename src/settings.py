c8bef0bde7faf63795b9234c157c343a3ab16e82
valentyn <valik@esmedia.es>
2022-03-07 23:15:33 +0200

    fix redis host

--- src/settings.py
+++ src/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://{}:6379/1".format(REDIS_HOST),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
